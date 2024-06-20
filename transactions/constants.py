d6115af699f31f4b410262ca4e383b24ba239e07
rafiulislamcse29 <rafiulislamcse29@gmail.com>
2024-06-20 10:47:40 -0700

    done

---
+++ transactions/constants.py
@@ -0,0 +1,16 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+TRANSFER_IN=5
+TRANSFER_OUT=6
+
+
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (TRANSFER_IN, 'Transfer In'),
+    (TRANSFER_OUT, 'Transfer Out'),
+)
